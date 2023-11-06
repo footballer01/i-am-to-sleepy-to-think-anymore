6b0f249ed7edcfbd34ae2776a5c5a50f29e895bf
footballer01 <134261271+footballer01@users.noreply.github.com>
2023-11-06 21:57:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function startClassification()
+{
+    navigator.mediaDevices.getUserMedia({audio: true});
+    classifier = ml5.soundClassifier( 'https://teachablemachine.withgoogle.com/models/CwHBlpCvL/modelReady' , modelReady);
+}
+
+
+
+
+function modelReady()
+{
+    classifier.classify(gotResults);
+}
